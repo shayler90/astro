eb14984a5d43850be45f96ffdf10a9fadeec2e98
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2016-08-15 17:26:14 -0400

    successfully fetch application name from toml file

--- astro.go
+++ astro.go
@@ -5,12 +5,12 @@ import (
 	"log"
 )
 
-func getApp() string {
-	config, err := toml.LoadFile("examples/basic_example.toml")
+func getApp(tomlfile string) string {
+	tree, err := toml.LoadFile(tomlfile)
 	if err != nil {
 		log.Fatal(err)
 	}
 	var app_name string
-	app_name = config.Get("application.name").(string)
+	app_name = tree.Get("application.name").(string)
 	return app_name
 }
--- astro_test.go
+++ astro_test.go
@@ -3,12 +3,8 @@ package astro
 import "testing"
 
 func TestGetApp(t *testing.T) {
-	config, err := toml.LoadFile("examples/basic_example.toml")
-	if err != nil {
-		t.Error(err)
-	}
-	app := getApp(config)
-	if app != "astro" {
-		t.Error("Expected 'astro', Got '%v'", app)
+	app_name := getApp("examples/basic_example.toml")
+	if app_name != "astro" {
+		t.Error("Expected: astro, Got: ", app_name)
 	}
 }
