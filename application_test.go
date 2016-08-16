59b8305ff89025c6e6138d05351c79b626707596
Devin Nathan-Turner <devin.nathan-turner@thinkresearch.com>
2016-08-15 20:17:20 -0400

    fix application

--- application_test.go
+++ application_test.go
@@ -3,7 +3,7 @@ package application
 import "testing"
 
 func TestGetName(t *testing.T) {
-	app_name := getApp("examples/basic_example.toml")
+	app_name := getName("examples/basic_example.toml")
 	if app_name != "astro" {
 		t.Error("Expected: astro, Got: ", app_name)
 	}
